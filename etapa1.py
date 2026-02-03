97ffe7a6d332719e8e83a6cb7f39e8f4e2723565
Bruno Doutor <bdoutor@gmail.com>
2026-02-03 10:20:42 +0000

    Primeira versão da etapa 1

---
+++ etapa1.py
@@ -0,0 +1,26 @@
+# Guarda nome Pedro na variavel Name
+# Guarda idade 45 na variavel Age
+# Guarda valor booleano True na variavel Is_Smoker
+name = "Bruno"
+age = 45
+is_smoker = False
+height = 1.71
+
+
+print("Name:", name)
+print("Age:", age)
+
+if is_smoker:
+    print("Note: smoker")
+else:
+    print("Note: non-smoker")
+
+print("Height:", height)
+
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor") 
+
+age_in_5_years = age + 5
+print("In 5 years you will be:", age_in_5_years)
